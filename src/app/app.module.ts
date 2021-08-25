3c8e8f258f704476269c0b6d41bf8e1bf71b471d
dmitry-averbukh <59136584+dmitry-averbukh@users.noreply.github.com>
2021-08-25 23:28:03 +0300

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactComponent } from './contact/contact.component';
+import { ContactListComponent } from './contact/contact-list/contact-list.component';
+import { ContactDetailComponent } from './contact/contact-detail/contact-detail.component';
+import { ContactItemComponent } from './contact/contact-list/contact-item/contact-item.component';
+import { MeetingsComponent } from './meetings/meetings.component';
+import { MitingEditComponent } from './meetings/miting-edit/miting-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContactComponent,
+    ContactListComponent,
+    ContactDetailComponent,
+    ContactItemComponent,
+    MeetingsComponent,
+    MitingEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/contact/contact-list/contact.model.ts
@@ -0,0 +1,11 @@
+export class Contact{
+  name: string;
+  description: string;
+  imgUrl: string;
+
+  constructor(name: string, description: string, imgUrl: string) {
+    this.name = name;
+    this.description = description;
+    this.imgUrl = imgUrl;
+  }
+}
